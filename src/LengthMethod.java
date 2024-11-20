627aa2000f9dae61d1f44b896d9f8fed3fbc2de2
Prince Baidoe <Princeba11doe@gmail.com>
2024-11-20 13:43:33 -0500

    Initial commit

---
+++ src/LengthMethod.java
@@ -0,0 +1,17 @@
+public class LengthMethod {
+    public static void lengthSize(String[] args) {
+        String str1 = "Java";
+        String str2 = "";
+
+
+
+
+            public void length(){
+            System.out.println(str1.length());
+            System.out.println(str2.length());
+            System.out.println("Java".length());
+            System.out.println("Java\n".length());
+            System.out.println("Learn Java".length());
+        }
+    }
+}
